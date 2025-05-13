1d11b0e9f252f44e084e71cbf27c9b2bee75a57c
FuTina <tina.fuchs@t-online.de>
2025-05-13 11:25:14 +0200

    adapt py script; add default config

--- configs/redis-secure-demo/import.py
+++ configs/redis-secure-demo/import.py
@@ -1,51 +1,33 @@
 import csv
-import redis
 import os
 from dotenv import load_dotenv
+import redis
 
 # Load .env variables
 load_dotenv()
 
-host = os.getenv("REDIS_HOST", "localhost")
+host = os.getenv("REDIS_HOST", "127.0.0.1")
 port = int(os.getenv("REDIS_PORT", 6379))
 username = os.getenv("REDIS_APPUSER_USERNAME", "appuser")
 password = os.getenv("REDIS_APPUSER_PASSWORD")
 
-
-print("🔌 Connecting with:")
-print("  host:", os.getenv("REDIS_HOST"))
-print("  port:", os.getenv("REDIS_PORT"))
-print("  username:", os.getenv("REDIS_APPUSER_USERNAME"))
-print("  password:", os.getenv("REDIS_APPUSER_PASSWORD"))
-
-
-try:
-    r = redis.StrictRedis(
-        host=host,
-        port=port,
-        username=username,
-        password=password,
-        decode_responses=True
-    )
-    print("🔁 Testing connection with PING...")
-    print("✅ Redis PING response:", r.ping())
-except redis.exceptions.ConnectionError as e:
-    print("❌ Redis connection failed:", e)
-    exit(1)
-except redis.exceptions.AuthenticationError as e:
-    print("❌ Authentication failed:", e)
-    exit(1)
-
-
-r = redis.StrictRedis(
-    host=os.getenv("REDIS_HOST", "127.0.0.1"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
-    username=os.getenv("REDIS_APPUSER_USERNAME"),
-    password=os.getenv("REDIS_APPUSER_PASSWORD"),
+# try:
+pool = redis.ConnectionPool(
+    host=host,
+    port=port,
+    username=username,
+    password=password,
     decode_responses=True
 )
-
-print(r.ping())  # Sollte "True" zurückgeben
+r = redis.Redis(connection_pool=pool)
+#     print("🔁 Testing connection with PING...")
+#     print("✅ Redis PING response:", r.ping())
+# except redis.exceptions.ConnectionError as e:
+#     print("❌ Redis connection failed:", e)
+#     exit(1)
+# except redis.exceptions.AuthenticationError as e:
+#     print("❌ Authentication failed:", e)
+#     exit(1)
 
 
 print(f"🔌 Connecting to Redis at {host}:{port}...")
